c725628b43a35bddf63c22c3002674c524bb2d43
Enigma-Deez <cosmosjay21@gmail.com>
2025-11-22 18:32:00 +0100

    Checkpoint

--- aubsf/src/pages/Give.jsx
+++ aubsf/src/pages/Give.jsx
@@ -1,44 +1,39 @@
-import { Link } from "react-router-dom";
 import "../styles/global.css";
 import "../styles/give.css";
+
 export default function Give() {
   return (
-<div className="wrapper">
-    {/* <!-- Page Replica Section --> */}
-<section className="replica-section">
-  <div className="hero-replica">
-    <br /><br /><br /><br /><br /><br /><br />
-    <h2>"For all things come from You, and of your own we have given you."</h2>
-    <p><em>1 Chronicles 29:14</em></p>
-    <div className="replica-content">
-      <h3>Support the Ministry of AU-BSF</h3>
-      <p>We invite you to partner with us in spreading truth, beauty, and goodness through your generous giving.</p>
-      <br /><br /><br /><br /><br />
-     </div>
-  </div>
-</section>
-{/* <!-- Ways to Give Section --> */}
-<section className="ways-to-give">
-  <div className="give-container">
-    <h2>Ways to Give</h2>
-    <p>If you are interested in leaving a bequest to AU-BSF, please contact us at <a href="mailto:aubsf@gmail.com">aubsf@gmail.com</a> or call +234-906-85696.</p>
-{/* <!-- For donations and tithes and offerings be very specific --> */}
-    <div className="give-method">
-      {/* <i className="bx bx-bank"></i>  <!-- Add this icon line --> */}
-      <h3>Direct Deposit</h3>
-      <p><strong>AU-BSF Church Account</strong><br />
-      BSB: 032 032<br />
-      A/N: 811 496</p>
+    <div className="wrapper">
+      <section className="give-hero">
+        <div>
+          <h2>"For all things come from You, and of your own we have given you."</h2>
+          <p><em>1 Chronicles 29:14</em></p>
+          <div className="give-hero-content">
+            <h3>Support the Ministry of AU-BSF</h3>
+            <p>We invite you to partner with us in spreading truth, beauty, and goodness through your generous giving.</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="ways-to-give">
+        <div className="give-container">
+          <h2>Ways to Give</h2>
+          <p>If you are interested in leaving a bequest to AU-BSF, please contact us at <a href="mailto:aubsf@gmail.com">aubsf@gmail.com</a> or call +234-906-85696.</p>
+
+          <div className="give-method">
+            <h3>Direct Deposit</h3>
+            <p><strong>AU-BSF Church Account</strong><br />
+            BSB: 032 032<br />
+            A/N: 811 496</p>
+          </div>
+          
+          <div className="give-method">
+            <h3>Cheque</h3>
+            <p>Payable to <strong>AU-BSF Church</strong><br />
+            Post: Law Building Class IV, Achievers University Campus, Owo, Ondo State, Nigeria</p>
+          </div>
+        </div>
+      </section>
     </div>
-    
-    <div className="give-method">
-      {/* <i className="bx bx-credit-card"></i>  <!-- Add this icon line --> */}
-      <h3>Cheque</h3>
-      <p>Payable to <strong>AU-BSF Church</strong><br />
-      Post: Law Building className IV, Achievers University Campus, Owo, Ondo State, Nigeria</p>
-    </div>
-  </div>
-</section>
-</div>
   );
 }
